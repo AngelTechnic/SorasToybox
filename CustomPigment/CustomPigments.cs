8234e00874a6769e43fbacb57fd4f83ac1c2fe0c
Wavetamer <icbpenguin@gmail.com>
2026-05-18 00:14:19 -0700

    regen nerf on what's coming. also preparing for broken pigment implementation, thanks wolfacola

---
+++ CustomPigment/CustomPigments.cs
@@ -0,0 +1,31 @@
+﻿using BrutalAPI;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace SorasToybox
+{
+    public class CustomPigments
+    {
+        public static void Add()
+        {
+            if (!LoadedDBsHandler.PigmentDB._PigmentPool.ContainsKey("Broken"))
+            {
+                ManaColorSO BrokenPigment = ScriptableObject.CreateInstance<ManaColorSO>();
+                BrokenPigment.canGenerateMana = true;
+                BrokenPigment.dealsCostDamage = true;
+                BrokenPigment.pigmentID = "Broken";
+                BrokenPigment.manaSprite = ResourceLoader.LoadSprite("BrokenMana", null, 32, null);
+                BrokenPigment.manaUsedSprite = ResourceLoader.LoadSprite("BrokenManaUsed", null, 32, null);
+                BrokenPigment.manaCostSelectedSprite = ResourceLoader.LoadSprite("BrokenManaCostSelected", null, 32, null);
+                BrokenPigment.manaCostSprite = ResourceLoader.LoadSprite("BrokenManaCostUnselected", null, 32, null);
+                BrokenPigment.manaSoundEvent = "event:/BrokenPigmentGen";
+                BrokenPigment.healthSprite = ResourceLoader.LoadSprite("BrokenManaHealth", null, 32, null);
+                BrokenPigment.pigmentTypes = new List<string> { "Broken" };
+                Pigments.AddNewPigment("Broken", BrokenPigment);
+            }
+        }
+    }
+}
---
+++ Enemies/SEARCH.cs
@@ -0,0 +1,20 @@
+﻿using BrutalAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SorasToybox.Enemies
+{
+    public class SEARCH
+    {
+        public static void Add()
+        {
+            Enemy search = new Enemy("SEARCH", "SEARCH_EN")
+            {
+                Health = 8,
+                HealthColor = Pigments.Red
+            };
+        }
+    }
+}
