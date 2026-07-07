2c82564a793dae0a7469411e2ec719bf5b5d354d
Wavetamer <icbpenguin@gmail.com>
2026-07-07 13:36:13 -0700

    forgor to change the item sprite

--- Items/Vanilla Fool DM Unlocks/DriveItem.cs
+++ Items/Vanilla Fool DM Unlocks/DriveItem.cs
@@ -34,7 +34,7 @@ namespace SorasToybox.Items
                 Name = "Drive",
                 Flavour = "\"Sometimes you're stuck in the ground, and you never even knew it.\"",
                 Description = "On turn start, apply 2 Overclock to all party members that are standing in Constricted.\nOn turn end, apply 2 Constricted to this party member's slot.",
-                Icon = ResourceLoader.LoadSprite("item_lucifershead"),
+                Icon = ResourceLoader.LoadSprite("item_drive"),
                 TriggerOn = TriggerCalls.OnTurnFinished,
                 OnUnlockUsesTHE = false,
                 IsShopItem = false,
